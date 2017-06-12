014b11c8401b6429817c27bda145c2314e0f863d
William W. Chiang <william.pharmd@gmail.com>
2017-06-12 09:56:56 -0500

    Problem sets completed

--- loops.js
+++ loops.js
@@ -35,9 +35,60 @@ function sleep(milliseconds) {
 while(count < name.length) {
 
     console.log("Gimme a " + name[count] + "!!!");
-    sleep(2000);
+    //sleep(2000);
     count++;
 }
 console.log("WHAT'S THAT SPELL???");
-sleep(2000);
-console.log(name + "!!!");
+//sleep(2000);
+console.log(name.toUpperCase() + "!!!");
+
+    // Console logs the following:
+    // Gimme a W!!!
+    // Gimme a i!!!
+    // Gimme a l!!!
+    // Gimme a l!!!
+    // Gimme a i!!!
+    // Gimme a a!!!
+    // Gimme a m!!!
+    // WHAT'S THAT SPELL???
+    // William!!!
+
+// Problem sets
+
+// Print all numbers between -10 and 19
+
+var initializedNumber = -10;
+while(initializedNumber < 20) {
+    console.log(initializedNumber)
+    initializedNumber++
+}
+
+// Then print all even numbers between 10 and 40
+
+var anotherNumber = 10;
+while(anotherNumber < 41) {
+    if(anotherNumber % 2 == 0) {
+        console.log(anotherNumber)
+    }
+    anotherNumber++
+}
+
+// And then print all odd numbers between 300 and 333
+
+var moreNumbers = 300;
+while(moreNumbers < 335) {
+    if(moreNumbers % 2 == 1) {
+        console.log(moreNumbers);
+    }
+    moreNumbers++
+}
+
+// Finally, print all numbers divisible by 5 AND 3 between 5 and 50
+
+var startingNumber = 5;
+while(startingNumber <= 50) {
+    if(startingNumber % 5 == 0 && startingNumber % 3 == 0) {
+        console.log(startingNumber);
+    }
+    startingNumber++
+}
