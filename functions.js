80e550fe3487b090dbbc55e1de7375f08588c417
William W. Chiang <william.pharmd@gmail.com>
2017-06-13 00:26:44 -0500

    Done.

---
+++ functions.js
@@ -0,0 +1,78 @@
+// FUNCTIONS and moar!!!
+
+// Playground... if this IDE had something like Apple's xCode...
+
+function chant() {
+    console.log("Hey there!!!");
+};
+
+
+for(i=2;i<=5;i++) {
+    chant(i);
+};
+
+function sayHelloTo(name) {
+  console.log("Hello " + name + "!!!");
+}
+
+sayHelloTo("William");
+
+function acquirePatientData(name, medications, insurance) {
+    console.log(medications);
+
+    console.log(name + " with the insurance called: " + insurance);
+}
+
+acquirePatientData("William", ["drug", "drug2"]);
+
+var square = function(num) {
+    return num * num;
+}
+
+console.log(square(2));
+
+
+// Functional problem sets... pun intended ... :P
+
+// isEven()
+
+function isEven(number) {
+    if(number % 2 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+// piece of cake...
+
+
+// factorial
+// Write a function which takes a single numeric argument and returns the factorial of that number...
+// oh boy.......
+
+function factorial(ofNumber) {
+    if(ofNumber == 0) {
+        return 1;
+    }
+
+    if(ofNumber != Math.floor(ofNumber)) {
+        return "Give me an integer please!!!";
+    }
+
+    var totalValue = 1;
+    for(integer = 1; integer <= ofNumber; integer++) {
+        totalValue *= integer;
+    }
+    return totalValue;
+}
+console.log("*****") ;
+console.log(factorial(10)); // i did it, yay :] !!!
+
+
+// kebabToSnake ... huh???
+// Write a function which takes a single kebab-based string argument and returns the snake_cased version
+
+function kebabToSnake(string) {
+    return string.replace(/-/g, "_");
+}
+console.log(kebabToSnake("hello-world")); //easy like pie
